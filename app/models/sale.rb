b45aaf4acd41735b62621f0865aa073383578529
anthony gombert <anthonygombert33@gmail.com>
2023-03-06 17:58:27 +0100

    sales model ok

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,5 +2,5 @@ class Sale < ApplicationRecord
   belongs_to :product
 
   validates :address, :sale_capacity, :end_date, :price_reduction, :progress, presence: true
-  enum :progress, { on_progress: 0, validate: 1, done: 2, cancel: 3 }
+  enum :progress, { in_progress: 0, confirmed: 1, done: 2, cancel: 3 }
 end
