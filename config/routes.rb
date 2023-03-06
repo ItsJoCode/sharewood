daca432a0c2b24412ef6e9262e595728c706c1ee
Jonathan Bergerot <jobergerot@gmail.com>
2023-03-06 23:20:28 +0100

    add owner namespaces with views and controller + route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "sales#index"
+  namespace :owner do
+    resources :products, only: %i[index show new create]
+  end
   resources :products, only: %i[show]
   resources :sales, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
