f088eed40aa080e080d49afc6c845053e26c7097
Jonathan Bergerot <jobergerot@gmail.com>
2023-03-09 15:00:32 +0100

    add js for cursor_bar

--- app/javascript/controllers/range_slider_controller.js
+++ app/javascript/controllers/range_slider_controller.js
@@ -2,17 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="range-slider"
 export default class extends Controller {
-  static targets = [ "output", "input" ]
+  static targets = ["output", "input", "price", "weight"]
+  static values = { price: Number, weight: Number }
 
   connect() {
-    this.updateOutput()
+    // this.updateOutput()
+    console.log(this.priceValue)
+    console.log(this.weightValue)
   }
 
   updateOutput() {
-    this.outputTarget.textContent = this.inputTarget.value
+    this.outputTarget.textContent = Number.parseInt(this.inputTarget.value, 10);
+    this.displayPrice()
+    this.displayWeight()
   }
 
   onInput() {
     this.updateOutput()
+  }
+
+  displayPrice() {
+    this.priceTarget.textContent = this.priceValue * this.outputTarget.textContent
+  }
+
+  displayWeight() {
+    this.weightTarget.textContent = this.weightValue * this.outputTarget.textContent
   }
 }
