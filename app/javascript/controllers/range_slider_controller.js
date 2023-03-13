386ae8e865de0a9728afa6361475df7a8653b182
Florent ARENE <florent.arene@gmail.com>
2023-03-13 18:25:06 +0100

    befor pull progress bar

--- app/javascript/controllers/range_slider_controller.js
+++ app/javascript/controllers/range_slider_controller.js
@@ -2,19 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="range-slider"
 export default class extends Controller {
-  static targets = ["output", "input", "price", "weight"]
+  static targets = ["output", "input", "price", "weight", "draw"]
   static values = { price: Number, weight: Number }
 
   connect() {
     // this.updateOutput()
-    console.log(this.priceValue)
-    console.log(this.weightValue)
+    // console.log(this.priceValue)
+    // console.log(this.weightValue)
   }
 
   updateOutput() {
-    this.outputTarget.textContent = Number.parseInt(this.inputTarget.value, 10);
-    this.displayPrice()
-    this.displayWeight()
+    const sliderValue = Number.parseInt(this.inputTarget.value, 10);
+    this.outputTarget.textContent = sliderValue;
+
+    const draw = this.drawTarget;
+    const circles = draw.querySelectorAll("i.fa-circle");
+    circles.forEach((circle, index) => {
+      if (index < sliderValue) {
+        circle.classList.add("fa-solid");
+        circle.classList.remove("fa-regular");
+      } else {
+        circle.classList.remove("fa-solid");
+        circle.classList.add("fa-regular");
+      }
+    });
+
+    this.displayPrice();
+    this.displayWeight();
   }
 
   onInput() {
